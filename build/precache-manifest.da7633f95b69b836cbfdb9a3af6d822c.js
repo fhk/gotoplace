e3a9c09e09f8e30b6805e8379c3588f7e8d4821a
Fabion Kauker <f.kauker@gmail.com>
2019-03-01 10:09:53 -0800

    init repo

---
+++ build/precache-manifest.da7633f95b69b836cbfdb9a3af6d822c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0f1d40f63ae8579c81db",
+    "url": "/static/css/main.0e4cfc77.chunk.css"
+  },
+  {
+    "revision": "0f1d40f63ae8579c81db",
+    "url": "/static/js/main.0f1d40f6.chunk.js"
+  },
+  {
+    "revision": "74ab97657c0e60fe520c",
+    "url": "/static/css/1.cecb4011.chunk.css"
+  },
+  {
+    "revision": "74ab97657c0e60fe520c",
+    "url": "/static/js/1.74ab9765.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b76e22f2bac84e9cb556f4633a5ba67f",
+    "url": "/index.html"
+  }
+];
